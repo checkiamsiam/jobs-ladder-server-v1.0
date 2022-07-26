a3d823e1a700be0cf579b1f9f705814163eb13d5
checkiamsiam <issiam02415@gmail.com>
2022-07-26 23:31:03 +0600

    user-added

--- index.js
+++ index.js
@@ -3,32 +3,17 @@ const express = require('express')
 const cors = require('cors');
 const app = express()
 const port = process.env.PORT || 5000
-const jwt = require('jsonwebtoken');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config()
+const userRouter = require('./routes/userRouter/userRouter');
+
+
 
 // middle ware
 app.use(express.json())
 app.use(cors())
 
-// database access
-const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.a8gmi.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri);
 
-
-// body of the program
-async function run() {
-  try {
-   
-   
-
-  } finally {
-  }
-};
-
-
-// call the async server
-run().catch(console.dir);
+// routers
+app.use('/users' , userRouter)
 
 
 app.get('/', (req, res) => {
---
+++ middlewares/verifyJWT.js
@@ -0,0 +1,17 @@
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: 'UnAuthorized access' });
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: 'Forbidden access' })
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
+
+module.exports = verifyJWT ;
