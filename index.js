269010fcbe3f46672fd18e5564389b6f09b3e9b7
checkiamsiam <issiam02415@gmail.com>
2022-08-06 18:47:49 +0600

    company router

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const app = express();
 const port = process.env.PORT || 5000;
 const userRouter = require("./routes/userRouter/userRouter");
 const jobPostRouter = require("./routes/jobPostRouter/jobPostRouter");
+const companyRouter = require("./routes/companyRouter/companyRouter");
 
 // middle ware
 app.use(express.json());
 app.use(cors());
 
 // routers
+app.use("/company", companyRouter);
 app.use("/users", userRouter);
 app.use("/job-post", jobPostRouter);
 
--- routes/companyRouter/companyRouter.js
+++ routes/companyRouter/companyRouter.js
@@ -1,21 +1,30 @@
-const express = require('express');
+const express = require("express");
 const companyRouter = express.Router();
-const mongodb = require('../../features/mongodb');
-
+const mongodb = require("../../features/mongodb");
 
 async function run() {
-  const companyCollection = await mongodb.collection('Companies')
+  const companyCollection = await mongodb.collection("Companies");
   try {
-   
-    
-   
-
+    companyRouter.get("/", async (req, res) => {
+      const query = req.query;
+      const getCompanyDetails = await companyCollection.find(query).toArray();
+      res.send(getCompanyDetails);
+    });
+    companyRouter.put("/:secret", async (req, res) => {
+      const companySecret = req.params.secret;
+      const filter = { companySecret };
+      const options = { upsert: true };
+      const data = {
+        $set: req.body,
+      };
+      const update = await companyCollection.updateOne(filter, data, options);
+      res.send(update);
+    });
   } finally {
   }
-};
-
+}
 
 // call the async server
 run().catch(console.dir);
 
-module.exports = companyRouter ;
+module.exports = companyRouter;
